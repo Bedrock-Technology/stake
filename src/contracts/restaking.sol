f6905a6bb54fdd21fdc2a0c3dad950da00dd7627
fuli <fuli@rockx.com>
2024-01-17 12:32:10 +0800

    init

--- src/contracts/restaking.sol
+++ src/contracts/restaking.sol
@@ -23,12 +23,10 @@ import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol"
 import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 
-contract RockXRestaking is Initializable, PausableUpgradeable, AccessControlUpgradeable, ReentrancyGuardUpgradeable {
+contract RockXRestaking is Initializable, AccessControlUpgradeable, ReentrancyGuardUpgradeable {
     using SafeERC20 for IERC20;
     using Address for address payable;
     using Address for address;
-
-    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
 
     /// @dev the EigenLayer EigenPodManager contract
     address public eigenPodManager;
@@ -57,12 +55,10 @@ contract RockXRestaking is Initializable, PausableUpgradeable, AccessControlUpgr
     ) initializer public {
         require(_eigenPodManager != address(0x0), "SYS026");
 
-        __Pausable_init();
         __AccessControl_init();
         __ReentrancyGuard_init();
 
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
-        _grantRole(PAUSER_ROLE, msg.sender);
 
         eigenPodManager = _eigenPodManager;
         // Deploy new EigenPod
