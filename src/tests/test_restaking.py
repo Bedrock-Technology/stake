5b1f00a46818982d7abb07508a7b603a59a76503
fuli <fuli@rockx.com>
2024-01-18 14:52:57 +0800

    update test

--- src/tests/test_restaking.py
+++ src/tests/test_restaking.py
@@ -10,7 +10,9 @@ from brownie import *
 from conftest import deps
 
 """ test of restaking a validator """
-def test_restaking(setup_contracts, owner, deployer, pubkeys, sigs):
+def test_restaking(setup_contracts, owner, deployer):
+    pubkeys = [0x9739c7d31f92e4f23d8d9fad9396154255cc4644533e5cde6a1ab4f7ad37da15e748a1317f721658302f1cd1f2b234e5]
+    sigs = [0xb94da5641c955a3f4620329387010057ab4b5d7344b823a59607d960e8eeeebfe0f8164fac976c4ed97261b991c157d508ec31edc65f93ee1adfda16a25d20c457b247092fab287b3619b66f35277767cfa5a3e83d4499d7fceb690a5007be2d]
     transparent_xeth, transparent_staking, transparent_redeem = setup_contracts
 
     transparent_staking.registerRestakingValidators(pubkeys, sigs, {'from': owner})
@@ -35,9 +37,8 @@ def test_restaking(setup_contracts, owner, deployer, pubkeys, sigs):
     print("restaking address", transparent_staking.addrRestaking(),transparent_staking.restakingWithdrawalCredentials()) 
 
     # mint
+    assert transparent_staking.getNextValidatorId() == 0
     transparent_staking.toggleWhiteList(owner, {'from':owner})
     assert transparent_staking.isWhiteListed(owner) == True
     transparent_staking.mint(0, time.time() + 600, {'from':owner, 'value': '64 ether'})
-
-    # stake
-    transparent_staking.stake({'from':owner})
+    assert transparent_staking.getNextValidatorId() == 1
